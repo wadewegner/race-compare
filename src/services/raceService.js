2dfd76798fa47c591d0c05f961f98f62feef0195
Wade Wegner <wade.wegner@gmail.com>
2025-01-03 09:32:18 -0800

    still debugging

--- src/services/raceService.js
+++ src/services/raceService.js
@@ -11,7 +11,7 @@ class RaceService {
                     '--disable-setuid-sandbox',
                     '--disable-dev-shm-usage'
                 ],
-                executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null
+                executablePath: '/usr/bin/chromium-browser'
             };
 
             console.log('Launching browser with options:', JSON.stringify(options, null, 2));
